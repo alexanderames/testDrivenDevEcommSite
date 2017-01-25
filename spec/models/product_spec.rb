7fe51df771de731fd5b6d66aa2dc7076a151c0f7
alexander ames <alexanderthomasames@gmail.com>
2017-01-25 10:39:31 -0700

    write test so that quantity can't be negative

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   it "doesn't allow a product's quantity to be negative" do
+     macbook = FactoryGirl.build(:product, quantity: -1)
+     expect(macbook).to_not be_valid
+   end
 end
