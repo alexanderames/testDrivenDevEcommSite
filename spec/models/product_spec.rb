85f12646639ed218243b8e94092abc7e52db2035
alexander ames <alexanderthomasames@gmail.com>
2017-01-25 10:45:58 -0700

    write test for price and decimal point placement

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,8 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-   it "doesn't allow a product's quantity to be negative" do
-     macbook = FactoryGirl.build(:product, quantity: -1)
-     expect(macbook).to_not be_valid
-   end
+   describe "name" do
+    it "cannot be nil"
+      product = FactoryGirl.build(:product, name: nil)
+    end
+  end
+  
+  describe "quantity" do
+    it "cannot be nil" do
+      product = FactoryGirl.build(:product, quantity: nil)
+
+      expect(product).to_not be_valid
+    end
+
+    it "cannot be negative" do
+      product = FactoryGirl.build(:product, quantity: -1)
+
+      expect(product).to_not be_valid
+    end
+
+  describe "price" do
+    it "cannot have 3 or more numbers after the decimal point" do
+      product = FactoryGirl.build(:product, price: 78.456)
+
+      expect(product).to_not be_valid
+    end
+  end
 end
